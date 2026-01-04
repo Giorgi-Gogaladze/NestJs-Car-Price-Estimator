671222f30b942656652c0a92d96b620c5372d74a
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-01-04 15:50:11 +0400

    feat:(test) applying session cookie and validation pipe globally for e2e

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,11 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ClassSerializerInterceptor, ValidationPipe} from '@nestjs/common'
-const cookieSession = require('cookie-session')
+import { ClassSerializerInterceptor} from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    })
-  );
-  app.use(cookieSession({
-    keys: ['mycookieskey']
-    })
-  ),
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
