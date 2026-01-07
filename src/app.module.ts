486db139e80dc6c532cea0cbf044d42a10040b0f
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-01-07 19:52:42 +0400

    refactor: assining currentuser with middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -49,3 +49,5 @@ export class AppModule {
     .forRoutes('*')
   }
 }
+
+export class 
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -12,6 +12,9 @@ export class User{
     @Column()
     password: string;
 
+    @Column({default: true})
+    admin: boolean;
+
     @AfterInsert()
     logInserts(){
         console.log('inserted user with id', this.id)
